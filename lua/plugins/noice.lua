3a3698abd62a40b11e05393a9cab5ebb79e71d7a
Aleksandr Gurov <s1lent.ssh@gmail.com>
2024-12-07 15:30:27 +0100

    Updated noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -24,6 +24,10 @@ return {
             },
         },
         presets = {
+            bottom_search = false,
+            command_palette = true,
+            long_message_to_split = true,
+            inc_rename = true,
             lsp_doc_border = true,
         },
     },
